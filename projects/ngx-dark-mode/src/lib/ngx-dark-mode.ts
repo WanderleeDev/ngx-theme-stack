8577b7b6fa9bfa702dd55a0a5503bcd0d3f967c0
Wanderlee <wanderleedev@gmail.com>
2026-03-23 08:39:47 -0500

    chore: initial library setup and scoped package publish

---
+++ projects/ngx-dark-mode/src/lib/ngx-dark-mode.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-dark-mode',
+  imports: [],
+  template: `
+    <p>
+      ngx-dark-mode works!
+    </p>
+  `,
+  styles: ``,
+})
+export class NgxDarkMode {
+
+}
---
+++ projects/ngx-dark-mode/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-dark-mode
+ */
+
+export * from './lib/ngx-dark-mode';
