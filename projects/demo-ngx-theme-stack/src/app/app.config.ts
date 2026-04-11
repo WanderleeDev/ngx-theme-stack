272b047cde3b0e7a408f5d8c4d9fadea16d1bd9e
Wanderlee <wanderleedev@gmail.com>
2026-04-11 15:58:00 -0500

    refactor: reorder appConfig import in main.ts

--- projects/demo-ngx-theme-stack/src/app/app.config.ts
+++ projects/demo-ngx-theme-stack/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideThemeStack } from 'ngx-theme-stack';
 import { routes } from './app.routes';
 
@@ -14,7 +13,6 @@ export const appConfig: ApplicationConfig = {
         skipInitialTransition: true,
       }),
     ),
-    provideClientHydration(withEventReplay()),
     provideThemeStack({
       themes: ['sunset'],
     }),
